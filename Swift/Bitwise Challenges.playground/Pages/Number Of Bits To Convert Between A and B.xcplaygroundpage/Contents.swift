2775a3375c5af6814e8ea7199690e53bced142d5
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2018-06-28 14:28:20 -0700

    Add Swift playground for 'Number of bits required to convert X to Y' article

---
+++ Swift/Bitwise Challenges.playground/Pages/Number Of Bits To Convert Between A and B.xcplaygroundpage/Contents.swift
@@ -0,0 +1,48 @@
+//: [Previous](@previous)
+
+/*
+ 
+ Find 100% human-friendly explanation of this coding challenge at http://pabloduo.com/2018/06/29/
+ 
+ Given two integers X and Y, write a function to find out a number of bits required to convert one integer to the other.
+ 
+ EXAMPLE
+ 
+ Input: X = 0b1000; Y = 0b0011;
+ 
+ Output: 3
+
+ */
+
+func bitsToConvert(x: UInt, y: UInt) -> Int {
+    // Prepare state
+    var bits = 0, diff = x ^ y
+    // Loop thru diff until it runs out of bits
+    while diff > 0 {
+        // Plus one more bit that is different
+        bits += 1
+        // Flip the next least-significant bit
+        diff &= diff - 1
+    }
+    // This is how many bits are required (eq. different) to convert x to y
+    return bits
+}
+
+// Assert a few simple cases
+bitsToConvert(x: 0b0000, y: 0b0000) == 0
+bitsToConvert(x: 0b0001, y: 0b0000) == 1
+bitsToConvert(x: 0b0011, y: 0b0001) == 1
+bitsToConvert(x: 0b0010, y: 0b0001) == 2
+
+// Assert original case
+bitsToConvert(x: 0b1000, y: 0b0011) == 3
+
+// Assert a few edge cases using hexadecimal notation
+bitsToConvert(x: 0xffff_ffff_ffff_ffff, y: 0xffff_ffff_ffff_ffff) == 0
+bitsToConvert(x: 0xffff_ffff_ffff_ffff, y: 0xffff_ffff_ffff_fffe) == 1
+bitsToConvert(x: 0xffff_ffff_ffff_ffff, y: 0xffff_ffff_ffff_fff0) == 4
+bitsToConvert(x: 0xffff_ffff_ffff_ffff, y: 0x0000_0000_0000_0000) == 64
+bitsToConvert(x: 0xffff_ffff_ffff_ffff, y: 0x0000_0000_0000_0001) == 63
+
+
+//: [Next](@next)
