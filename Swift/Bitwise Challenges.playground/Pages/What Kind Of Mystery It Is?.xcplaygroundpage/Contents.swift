58849e21363d97b1bbf50507d197ddc051b4c3c8
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2018-06-27 19:20:36 -0700

    Add Swift playground for 'What that mystery function does?' article

---
+++ Swift/Bitwise Challenges.playground/Pages/What Kind Of Mystery It Is?.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+//: [Previous](@previous)
+
+/*
+ 
+ Find 100% human-friendly explanation of this coding challenge at http://pabloduo.com/2018/06/28/mystery-function
+ 
+ Explain what the following function does...
+ 
+ func mystery(n: Int) -> Bool {
+    // Some mystery bitwise computation going on...
+    return n & (n - 1) == 0
+ }
+ 
+ 
+ 
+ EXAMPLE
+ 
+ Input: n = 0b0000_1000; n = 0b0110_0101;
+ 
+ Output: true; false;
+ 
+ */
+
+func powerof2(n: Int) -> Bool {
+    // Find out whether the given number is a power of 2
+    return n & (n - 1) == 0
+}
+
+
+// Assert original use cases + few more
+powerof2(n: 0b0000_0100) == true  // (true)
+powerof2(n: 0b0000_1000) == true  // (true)
+powerof2(n: 0b0001_0000) == true  // (true)
+powerof2(n: 0b0110_0101) == false // (true)
+
+//: [Next](@next)
