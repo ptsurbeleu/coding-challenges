2d53c62298124eccaaf50a3b75496097e83e8ece
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2018-08-18 16:27:44 -0700

    Add Swift playground for 'Clear the least significant bit' article

---
+++ Swift/Bitwise Challenges.playground/Pages/Clear The Least Significant Bit.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+//: [Previous](@previous)
+
+/*
+
+ Write a function to clear the least significant bit in the specified number.
+ 
+ 
+ EXAMPLE:
+ 
+    Input: x = 0b1100
+ 
+    Output: 0b1000
+ 
+ */
+
+/// Clears the least significant bit (LSB).
+///
+func clear(x: Int) -> Int {
+    // Convert into unsigned integer to work with bits
+    let xx = UInt(bitPattern: x)
+    // Here is the answer
+    return Int(bitPattern: xx & (xx - 1))
+}
+
+// Assert
+clear(x:  0b1100) == 0x8
+clear(x: 1 << 63) == 0x0
+clear(x: 1 <<  0) == 0x0
+clear(x: 3 <<  0) == 0x2
+clear(x: 3 << 62) == Int.min
+clear(x: 1 << 34) == 0x0
+
+//: [Next](@next)
