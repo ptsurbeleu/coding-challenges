a80b9951b7d0936f2998816d3d10e4f2a072f5c0
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2018-08-23 00:15:15 -0700

    Fix an issue with negative numbers for 'Compute X * 3.5 Using Bitwise Operators' playground + Update article public location

--- Swift/Bitwise Challenges.playground/Pages/Compute X * 3.5 Using Bitwise Operators.xcplaygroundpage/Contents.swift
+++ Swift/Bitwise Challenges.playground/Pages/Compute X * 3.5 Using Bitwise Operators.xcplaygroundpage/Contents.swift
@@ -1,6 +1,8 @@
 //: [Previous](@previous)
 
 /*
+ 
+ Find a human-friendly explanation of this coding challenge at http://pabloduo.com/2018/08/22/
 
  Write a function to compute 3.5x using bitwise operators only.
 
@@ -18,31 +20,38 @@
 func multiply(x: Int) -> Int {
     // Prepare state of the algorithm
     var xx = x << 1, yy = x, zz = x >> 1
-    // Iterate over 2nd and 3rd operands
-    while yy > 0 || zz > 0 {
-        // Query carry of 1st and 2nd operands
+    // Iterate over 2nd and 3rd terms
+    while yy != 0 || zz != 0 {
+        // Query carry of 1st and 2nd terms
         let cy = xx & yy
-        // Add 1st and 2nd operands together
+        // Add 1st and 2nd terms together
         xx ^= yy
-        // Shift carry of 1st and 2nd operands
+        // Shift carry of 1st and 2nd terms
         yy = cy << 1
-        // Query carry of 3rd operand
+        // Query carry of 3rd term
         let cz = xx & zz
-        // Add 3rd operand to the sum
+        // Add 3rd term to the sum
         xx ^= zz
-        // Shift carry of 3rd operand
+        // Shift carry of 3rd term
         zz = cz << 1
     }
     // Here is the answer
     return xx
 }
 
-// Assert a few use cases
-multiply(x:  0) ==  0
-multiply(x:  2) ==  7
-multiply(x:  4) == 14
-multiply(x:  6) == 21
-multiply(x:  8) == 28
-multiply(x: 10) == 35
+// Assert a few positive use cases
+multiply(x:  0) ==  0 // true
+multiply(x:  2) ==  7 // true
+multiply(x:  4) == 14 // true
+multiply(x:  6) == 21 // true
+multiply(x:  8) == 28 // true
+multiply(x: 10) == 35 // true
+
+// Assert a few negative test cases
+multiply(x:  -2) ==  -7 // true
+multiply(x:  -4) == -14 // true
+multiply(x:  -6) == -21 // true
+multiply(x:  -8) == -28 // true
+multiply(x: -10) == -35 // true
 
 //: [Next](@next)
