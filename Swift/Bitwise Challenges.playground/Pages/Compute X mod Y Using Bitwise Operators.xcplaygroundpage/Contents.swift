7784e73c68f677eb86fdff7e2520b73857cb9a67
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2018-08-27 14:45:46 -0700

    Editorial tweaks on 'Compute X mod Y Using Bitwise Operators' Swift playground

--- Swift/Bitwise Challenges.playground/Pages/Compute X mod Y Using Bitwise Operators.xcplaygroundpage/Contents.swift
+++ Swift/Bitwise Challenges.playground/Pages/Compute X mod Y Using Bitwise Operators.xcplaygroundpage/Contents.swift
@@ -2,6 +2,8 @@
 
 /*
  
+ Find a human-friendly explanatio of this coding challenge at http://pabloduo.com/2018/08/26/
+ 
  Write a function to compute X mod Y without using division (/) and modulo (%) operators, where Y is a power of 2.
  
  
@@ -14,6 +16,7 @@
  */
 
 /// Computes X mod Y, where Y is a power of 2.
+///
 func mod(x: Int, y: Int) -> Int {
     // Convert into unsigned integers
     let xx = UInt(bitPattern: x), yy = UInt(bitPattern: y)
@@ -21,9 +24,20 @@ func mod(x: Int, y: Int) -> Int {
     return Int(bitPattern: xx & (yy - 1)) 
 }
 
-// Assert a few test cases
-mod(x: 6,   y: 4) == 6 % 4
-mod(x: 6,   y: 2) == 6 % 2
-mod(x: 887, y: 8) == 887 % 8
+// Assert a few simple test cases
+mod(x:    6, y:  4) ==    6 %  4 // true
+mod(x:    6, y:  2) ==    6 %  2 // true
+mod(x:  887, y:  8) ==  887 %  8 // true
+mod(x: 1033, y: 16) == 1033 % 16 // true
+
+// Assert a few edge cases
+mod(x: Int.max, y:  1) == Int.max %  1 // true
+mod(x: Int.max, y:  8) == Int.max %  8 // true
+mod(x: Int.max, y: 16) == Int.max % 16 // true
+mod(x: Int.max, y: 32) == Int.max % 32 // true
+mod(x: Int.min, y:  1) == Int.min %  1 // true
+mod(x: Int.min, y:  8) == Int.min %  8 // true
+mod(x: Int.min, y: 16) == Int.min % 16 // true
+mod(x: Int.min, y: 32) == Int.min % 32 // true
 
 //: [Next](@next)
