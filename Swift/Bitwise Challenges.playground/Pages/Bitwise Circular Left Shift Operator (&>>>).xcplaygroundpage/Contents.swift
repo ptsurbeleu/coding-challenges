f62ee1606a5bd264b7ad2987db35d9f43ce56c2b
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2018-08-27 18:29:04 -0700

    Editorial tweaks to 'Bitwise Circular Left Shift Operator (&>>>)' Swift playground

--- Swift/Bitwise Challenges.playground/Pages/Bitwise Circular Left Shift Operator (&>>>).xcplaygroundpage/Contents.swift
+++ Swift/Bitwise Challenges.playground/Pages/Bitwise Circular Left Shift Operator (&>>>).xcplaygroundpage/Contents.swift
@@ -2,12 +2,14 @@
 
 /*
  
- Write a function to implement bitwise circular left shift operator.
+ Find a human-friendly explanation of this coding challenge at http://pabloduo.com/2018/08/27/
+ 
+ Write a function to implement bitwise circular left shift `&>>>` operator.
  
  
  EXAMPLE:
  
-    Input: x = 0x0000_0000_0000_00ff, y = 8
+    Input: 0x0000_0000_0000_00ff &>>> 8
  
     Output: 0xff00_0000_0000_0000
  
@@ -18,17 +20,18 @@ infix operator &>>>: BitwiseShiftPrecedence
 
 /// Bitwise circular left shift operator declaration implementation.
 ///
-/// - Parameter x: An integer to apply bitwise circular left shift operator to.
-/// - Parameter y: A number of bits to shift to the left.
-public func &>>>(x: Int, y: Int) -> Int {
+/// - Parameter lhs: An integer to apply the shift operator to.
+/// - Parameter rhs: A number of bits to shift to the left.
+public func &>>>(lhs: Int, rhs: Int) -> Int {
     // Calculate size of the type in bits
-    let typesize = MemoryLayout.size(ofValue: x) * 8
+    let typesize = MemoryLayout.size(ofValue: lhs) * 8
     // Safe-guard from out-of-bounds issues and throw runtime error
-    if y < 0 || y >= typesize { fatalError("Shift amount is out-of-bounds of type size in bits") }
+    if rhs < 0 || rhs >= typesize {
+        fatalError("Shift amount is out-of-bounds of type size in bits") }
     // Convert both signed numbers into unsigned ones
-    let xx = UInt(bitPattern: x), yy = UInt(bitPattern: y)
+    let xx = UInt(bitPattern: lhs), yy = UInt(bitPattern: rhs)
     // Safe-guard shift offset to stay within the range
-    let offset = yy == 0 ? yy : UInt(bitPattern: typesize - y)
+    let offset = yy == 0 ? yy : UInt(bitPattern: typesize - rhs)
     // Convert resulting bits into a signed integer
     return Int(bitPattern: (xx << offset) | (xx >> yy))
 }
@@ -41,7 +44,9 @@ public func &>>>(x: Int, y: Int) -> Int {
 0x0000_0000_0000_ffff &>>> 7 == µ(0xfe00_0000_0000_01ff)
 0x0000_0000_0000_00ff &>>> 8 == µ(0xff00_0000_0000_0000)
 
-// NOTE: Uncomment these lines to see the safe-guard in action (eq. runtime error)
+// NOTE: Uncomment these lines (one at a time) to see 
+// the safe-guard in action (eq. runtime error)
+//
 // 0x0000_0000_0000_ffff &>>> -1 == µ(0xfe00_0000_0000_01ff)
 // 0x0000_0000_0000_ffff &>>> 64 == µ(0xfe00_0000_0000_01ff)
 
