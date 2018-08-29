b6a01f03c1ff002f3d6422277c8d50d2fdf7bae7
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2018-08-29 08:44:00 -0700

    Editorial tweaks to 'Compute Sum Using Bitwise Operators' playground

--- Swift/Bitwise Challenges.playground/Pages/Compute Sum Using Bitwise Operators.xcplaygroundpage/Contents.swift
+++ Swift/Bitwise Challenges.playground/Pages/Compute Sum Using Bitwise Operators.xcplaygroundpage/Contents.swift
@@ -2,9 +2,9 @@
 
 /*
 
- Find 100% human-friendly explanation of this coding challenge at http://pabloduo.com/2018/07/05/
+ Find a human-friendly explanation of this coding challenge at http://pabloduo.com/2018/07/05/
 
- Write a program that computes sum of two signed integers X and Y. Keep in mind your code needs to meet the following constraints:
+ Write a program that computes sum of two signed integers `x` and `y`. Keep in mind your code needs to meet the following constraints:
 
     - use only bitwise operators "<<", ">>", "|", "&", "~" and "^";
     - use assignment operator "=";
@@ -13,31 +13,32 @@
 
  EXAMPLE
 
-    Input: X = 999, Y = 1
+    Input: x = 999, y = 1
 
     Output: 1000
 
  */
 
 /// Computes sum of two signed numbers using bitwise operators only.
+///
 func sum(x: Int, y: Int) -> Int {
     // Since function parameters in Swift are constants,
     // we declare internal variables that we can modify
-    var xx = x, yy = y
-    // Iterate on Y while there are bits to work with
-    while yy != 0 {
-        // Compute carry of X and Y, using AND, where
-        // carry is a set of common bits between X and Y
-        let carry = xx & yy
-        // Compute sum of bits X and Y, using XOR, where sum
-        // is a set of bits that are different between X and Y
-        xx = xx ^ yy
+    var lhs = x, rhs = y
+    // Iterate on y while there are bits to work with
+    while rhs != 0 {
+        // Compute carry of x and y, using AND, where
+        // carry is a set of common bits between x and y
+        let carry = lhs & rhs
+        // Compute sum of bits x and y, using XOR, where sum
+        // is a set of bits that are different between x and y
+        lhs = lhs ^ rhs
         // Carry is shifted by 1, that is what we usually do
         // with carry, eq. move to the next position
-        yy = carry << 1
+        rhs = carry << 1
     }
     // Here is our sum
-    return xx
+    return lhs
 }
 
 // Assert a few basic use cases
