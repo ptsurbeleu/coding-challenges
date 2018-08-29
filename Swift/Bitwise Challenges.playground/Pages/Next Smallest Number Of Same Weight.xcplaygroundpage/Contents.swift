69993ede44efdeb47418a8c60e41d3fdee8a9a49
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2018-08-28 17:21:54 -0700

    Editorial tweaks for 'Next Smallest Number Of Same Weight' playground

--- Swift/Bitwise Challenges.playground/Pages/Next Smallest Number Of Same Weight.xcplaygroundpage/Contents.swift
+++ Swift/Bitwise Challenges.playground/Pages/Next Smallest Number Of Same Weight.xcplaygroundpage/Contents.swift
@@ -2,7 +2,7 @@
 
 /*
  
- Find 100% human-friendly explanation of this coding challenge at http://pabloduo.com/2018/06/26/next-number-with-same-weight.
+ Find a human-friendly explanation of this coding challenge at http://pabloduo.com/2018/06/26/
  
  * Lets define "the weight" of a nonnegative integer x to be the number of bits that are set to 1 in its binary representation.
  
@@ -11,57 +11,57 @@
  
  EXAMPLE
  
- Input: num = 0b11011001111100 (13948)
+    Input: num = 0b11011001111100 (13948)
  
- Output: smallest = 0b11011001111010 (13946)
+    Output: 0b11011001111010 (13946)
  
  */
 
-func nextSmallest(num: Int) -> Int {
-    // Prepare state
-    var _sample = num, _num = num, x = 0, y = 1, i = 0, flip = false
+func nextSmallest(n: Int) -> Int {
+    // Prepare state of the algorithm
+    var query = n, nn = n, y = 1, x = 0, i = 0, flip = false
     // Loop thru until we run out of bits to inspect
-    while _sample > 0 {
+    while query > 0 {
         // Rightmost trailing zero terminates
-        if biton(_sample, 1) && bitoff(_sample, 0) {
+        if biton(query, 1) && bitoff(query, 0) {
             // Signal to flip bits & terminate the loop
             flip = true; break;
         }
         // Swap bits only if they are different
-        if bit(_num, x) != bit(_num, i) {
+        if bit(nn, x) != bit(nn, i) {
             // An in-place bits swap
-            _num ^= 1 << x | 1 << i
+            nn ^= 1 << x | 1 << i
             // Move on to the next bit
             i += 1
         }
         // Shift by 1 bit to the right
-        _sample >>= 1
+        query >>= 1
         // Move on to the next pair of bits
         x += 1; y += 1;
     }
     // Flip rightmost trailing 0 with 1
-    if flip { return _num ^ (1 << y | 1 << x) }
+    if flip { return nn ^ (1 << y | 1 << x) }
     // And in some cases there is literally nothing we can do
     return -1
 }
 
 // Assert a few very basic use cases
-nextSmallest(num: 0b0010) == 0b0001 // (true)
-nextSmallest(num: 0b0101) == 0b0011 // (true)
-nextSmallest(num: 0b1001) == 0b0110 // (true)
-nextSmallest(num: 0b1000) == 0b0100 // (true)
+nextSmallest(n: 0b0010) == 0b0001 // (true)
+nextSmallest(n: 0b0101) == 0b0011 // (true)
+nextSmallest(n: 0b1001) == 0b0110 // (true)
+nextSmallest(n: 0b1000) == 0b0100 // (true)
 
 // Assert a few edge use cases
-nextSmallest(num:   0)     == -1 // (true)
-nextSmallest(num:  -1)     == -1 // (true)
-nextSmallest(num: -11)     == -1 // (true)
-nextSmallest(num: Int.min) == -1 // (true)
+nextSmallest(n:   0)     == -1 // (true)
+nextSmallest(n:  -1)     == -1 // (true)
+nextSmallest(n: -11)     == -1 // (true)
+nextSmallest(n: Int.min) == -1 // (true)
 
 // Assert slightly more involved use cases
-nextSmallest(num: 0b10011110000011) == 0b10011101110000 // (true)
-nextSmallest(num: 0b10011110010011) == 0b10011110001110 // (true)
-nextSmallest(num: 0b10011010000010) == 0b10011010000001 // (true)
-nextSmallest(num: 0b10000000000001) == 0b01100000000000 // (true)
-nextSmallest(num: 0b11011001111100) == 0b11011001111010 // (true)
+nextSmallest(n: 0b10011110000011) == 0b10011101110000 // (true)
+nextSmallest(n: 0b10011110010011) == 0b10011110001110 // (true)
+nextSmallest(n: 0b10011010000010) == 0b10011010000001 // (true)
+nextSmallest(n: 0b10000000000001) == 0b01100000000000 // (true)
+nextSmallest(n: 0b11011001111100) == 0b11011001111010 // (true)
 
 //: [Next](@next)
