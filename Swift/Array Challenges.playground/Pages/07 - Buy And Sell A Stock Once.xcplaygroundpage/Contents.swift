86ae3e22237845f5c18769322bddea8ef4ea9dba
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2018-09-10 17:19:35 -0700

    Add Swift playground for 'Buy And Sell A Stock Once' article

---
+++ Swift/Array Challenges.playground/Pages/07 - Buy And Sell A Stock Once.xcplaygroundpage/Contents.swift
@@ -0,0 +1,57 @@
+//: [Previous](@previous)
+
+/// Daily stock price unit.
+typealias StockPrice = Double
+/// Profit made by selling stock.
+typealias Profit = Double
+
+/*
+ 
+ Find a human-friendly explanation of this coding challenge at http://pabloduo.com/2018/09/10
+ 
+ Write a function that takes an array of daily stock price and returns the maximum profit that could be made by buying and and then selling the stock.
+ 
+ 
+ EXAMPLE:
+ 
+    Input: n = [20,20,15,15,25]
+ 
+    Output: 10
+
+ */
+
+
+/// Finds out the maximum profit that could be made.
+///
+/// - Parameter n: A sequence of daily stock prices.
+func max(n: [StockPrice]) -> Profit {
+    // Prepare state of the algorithm
+    var smallest = StockPrice.max, profit: Profit = 0
+    // Keep enumerating each stock price
+    for price in n {
+        // Find out the difference so far
+        let today = price - smallest
+        // Find out the maximum profit so far
+        profit = max(profit, today)
+        // Find out the smallest price so far
+        smallest = min(smallest, price)
+    }
+    // Here is the answer
+    return profit
+}
+
+
+// Assert a few simple use cases
+max(n: [   0]) ?>> 0
+max(n: [  10]) ?>> 0
+max(n: [0,10]) ?>> 10
+
+// Assert a few larger use cases
+max(n: [20,25,18]) ?>> 5
+max(n: [20,20,15,15,25]) ?>> 10
+
+// Assert a few even larger use cases
+max(n: [310,315,275,295,260,270,290,230,255,250]) ?>> 30
+
+
+//: [Next](@next)
---
+++ Swift/Array Challenges.playground/Sources/Double.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+extension Double {
+    /// Gets maximum positive value. 
+    public static var max: Double {
+        get { return Double.greatestFiniteMagnitude }
+    }
+}
