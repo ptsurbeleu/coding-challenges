b70be6c0ae16a467b353ddbae1392c035ea3bf50
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2018-11-20 17:55:55 -0800

    Refactor "Sample Online Data" Swift playground so it uses less code to do the same

--- Swift/Array Challenges.playground/Pages/0D - Sample Online Data.xcplaygroundpage/Contents.swift
+++ Swift/Array Challenges.playground/Pages/0D - Sample Online Data.xcplaygroundpage/Contents.swift
@@ -28,19 +28,17 @@ typealias Sequence = [Int]
 /// - Parameter k: A number of elements to chose from the stream.
 func sample(stream: Stream, k: Int) -> Sequence {
     // Prepare state of the algorithm
-    var seed = k, seq = Sequence(repeating: 0, count: k)
-    // Query first k elements from the stream
-    for i in 0..<k { seq[i] = stream.next()! }
+    var seed = 0, seq = Sequence(repeating: 0, count: k)
     // Query stream for the next element (if any)
     while let next = stream.next() {
-        // Extend random generator's seed range by 1
-        // to account for the current element
-        seed += 1
         // Select position of the element in `0...seed`
-        let x = Int.random(in: 0...seed)
+        let x = k > seed ? seed : Int.random(in: 0...seed)
         // Generator yields a value between `0` and `k - 1` is
         // a signal to replace an existing element in the sequence 
         if k > x { seq[x] = next }
+        // Extend random generator's seed range by 1
+        // to account for the current element
+        seed += 1
     }
     // Here is the answer
     return seq
@@ -48,6 +46,8 @@ func sample(stream: Stream, k: Int) -> Sequence {
 
 
 // Assert a few test cases
+sample(stream: of(in:     1...4), k: 4)
+sample(stream: of(in:     1...5), k: 4)
 sample(stream: of(in:     1...9), k: 4)
 sample(stream: of(in:    1...19), k: 4)
 sample(stream: of(in:    1...29), k: 4)
@@ -59,5 +59,6 @@ sample(stream: of(in:    1...79), k: 4)
 sample(stream: of(in:    1...89), k: 4)
 sample(stream: of(in:    1...99), k: 4)
 sample(stream: of(in:   1...999), k: 4)
+
 
 //: [Next](@next)
