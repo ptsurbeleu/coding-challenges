e2db2aad333c5fcdd72bcd9dd1f6af9e8d67caeb
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2019-01-20 16:49:37 -0800

    Minor refactoring of the implementation + use newer assertion syntax with emojis for "02 - Increment an Arbitrary Precision Number" playground

--- Swift/Array Challenges.playground/Pages/02 - Increment an Arbitrary Precision Number.xcplaygroundpage/Contents.swift
+++ Swift/Array Challenges.playground/Pages/02 - Increment an Arbitrary Precision Number.xcplaygroundpage/Contents.swift
@@ -20,15 +20,15 @@
 /// - Parameter n: An array with the number's digits (left-to-right notation)
 func increment(n: [Int]) -> [Int] {
     // Prepare state of the algorithm
-    var nn = n, i = nn.count, carry = 1
+    var nn = n, carry = 1, head = n.count
     // Enumerate all digits of the number (begin with rightmost - ones)
-    while i > 0 {
+    while head > 0 {
         // Adjust the index counter
-        i -= 1
+        head -= 1
         // Calculate sum in the current position 
-        let sum = nn[i] + carry
+        let sum = nn[head] + carry
         // Extract ones from the sum
-        nn[i] = sum % 10
+        nn[head] = sum % 10
         // Extract carry from the sum
         carry = sum / 10
     }
@@ -39,13 +39,13 @@ func increment(n: [Int]) -> [Int] {
 }
 
 // Assert a few test cases
-increment(n:     [0]) ==     [1]
-increment(n:     [1]) ==     [2]
-increment(n:   [2,1]) ==   [2,2]
-increment(n:   [8,9]) ==   [9,0]
-increment(n:   [9,9]) == [1,0,0]
-increment(n: [1,9,9]) == [2,0,0]
-increment(n: [2,9,9]) == [3,0,0]
-increment(n: [3,9,9]) == [4,0,0]
+increment(n:     [0]) ?>>     [1]
+increment(n:     [1]) ?>>     [2]
+increment(n:   [2,1]) ?>>   [2,2]
+increment(n:   [8,9]) ?>>   [9,0]
+increment(n:   [9,9]) ?>> [1,0,0]
+increment(n: [1,9,9]) ?>> [2,0,0]
+increment(n: [2,9,9]) ?>> [3,0,0]
+increment(n: [3,9,9]) ?>> [4,0,0]
 
 //: [Next](@next)
